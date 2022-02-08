b31ecceb9868bbc3ca7930bcc84bf99f0243a569
Piotr <straszak.piotr@gmail.com>
2022-02-08 08:29:29 +0100

    Initial commit and section 7 of the course

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-list-udemy';
+  title = 'Lista Zadań'
+
+  get footer () {
+    return '2020 © Lista zadań,All rights reserved.'
+  }
 }
